5cdd9a9aa401176602575f21a4f13d766e6f97bc
Iris <icl5@cornell.edu>
2020-10-18 19:17:01 -0400

    added interface for board

--- board.ml
+++ board.ml
@@ -1,1 +1,11 @@
-(** *)
+module type board = sig
+  (** Represents the game board which consists of tiles *)
+  type 'a t
+
+  (** [create x] is a board of [x] tiles*)
+  val create : int -> 'a t
+
+  (** [to_tile x] is the tile at location x*)
+  val to_tile : int -> 'a
+
+end
