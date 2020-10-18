8a696a3f2f38cdad44fd58fcb7e49457b7f4bad4
Iris <icl5@cornell.edu>
2020-10-18 19:50:34 -0400

    revised specs for board.ml and board.mli

--- board.ml
+++ board.ml
@@ -4,8 +4,10 @@ type 'a t = 'a list list
 (** *)
 let create x = failwith "notfound"
 
-(** *)
-let to_tile x = failwith "notfound"
+(** [next_tile tile] is the list of adjacent tiles to [tile] *)
+(* should build something to match if length 1, go to the tile, otherwise
+   prompt user choice and then go to tile*)
+let next_tile = failwith "notfound"
 
 (** TODO: helper for generation tiles and path randomly *)
 let randPaths = failwith "notfound"
--- board.mli
+++ board.mli
@@ -4,5 +4,5 @@ type 'a t
 (** [create x] is a board of [x] tiles*)
 val create : int -> 'a t
 
-(** [to_tile x] is the tile at location x*)
-val to_tile : int -> 'a
+(** [next_tile tile] is the list of adjacent tiles to [tile]*)
+val next_tile : 'a -> 'a list
