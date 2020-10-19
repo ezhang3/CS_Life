078493aae56871962c40d99bb7d539486d90b0e8
Janice Ying Ji <jyj9@en-ci-cisugcl13..coecis.cornell.edu>
2020-10-18 20:08:45 -0400

    Makefile, .merlin, test, tile

---
+++ _build/board.mli
@@ -0,0 +1,8 @@
+(** Represents the game board which consists of tiles *)
+type 'a t
+
+(** [create x] is a board of [x] tiles*)
+val create : int -> 'a t
+
+(** [to_tile x] is the tile at location x*)
+val to_tile : int -> 'a
