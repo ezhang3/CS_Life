d64707f91685e9f790bc22bf334e598368399636
Allison Liu <al843@cornell.edu>
2020-10-18 21:50:07 -0400

    added some things in board

--- board.mli
+++ board.mli
@@ -1,15 +1,16 @@
 open Tile 
 
 (** Represents the game board which consists of tiles *)
-type 'a t
+type gameboard
 
-(** [create x] is a board of [x] tiles*)
-val create : int -> 'a t
+(** [create x] is a board with [x] tiles*)
+val create : int -> gameboard
 
-(** [next_tile t] is the tile after tile [t] on the board. *)
-val next_tile : Tile.tile -> Tile.tile
+(** [next_tile cur next game] determines if *)
+val valid_move : Tile.tile -> Tile.tile -> gameboard -> bool
 
 (** [to_tile x] is the tile at location x*)
 val to_tile : int -> 'a
-(** [next_tile tile] is the list of adjacent tiles to [tile]*)
+
+(** [next_tile tile] is the list of the tiles in its path [tile]*)
 val next_tile : 'a -> ('a -> 'a -> bool)-> 'a list
