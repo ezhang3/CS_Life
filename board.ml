ceefb9a1a90e5025ca8c8e46253ffe61c7a1fc05
Iris <icl5@cornell.edu>
2020-10-23 11:47:53 -0400

    added pseudocode for board create

--- board.ml
+++ board.ml
@@ -18,6 +18,18 @@ let tile = Tile.create_tile Red event ""
 
 let test_board = [tile, [tile]]
 
+(* how to create boards:
+   function to
+   Generate tiles from a list of different tiles with events
+   - automatically gives them a numerical string id by order of generation
+     -
+     tiles = [
+      [color, event, neighbors, numoccur];
+      [color, event, neighbors, numoccur];
+     ]
+     thinking we should add tile neighbors to the tile module
+     Generate board using those tiles *)
+
 let rec create x = failwith "notfound"
 (*
    match x with
@@ -25,13 +37,11 @@ let rec create x = failwith "notfound"
 | x -> create (x-1) (tile :: acc
 *)
 
-(** [find_tile tile func board] finds the tile with id [tile] in board [board]
-    that matches and gives the list of adjacent tiles ahead of it. 
-    [func tile1 tile2] is a function used to compare tiles *)
 let rec find_tile (tile : Tile.tile) func (board : gameboard) =
   match board with
   | [] -> failwith "notfound"
   | (a, b) :: t -> if func a tile then b else find_tile tile func t
 
-(** [next_tile tile] is the list of adjacent tiles to [tile] *)
+(** [next_tile tile func board] is the list of adjacent tiles to [tile]
+    [func tile1 tile2] is a function used to compare tiles *)
 let next_tile = find_tile
--- board.mli
+++ board.mli
@@ -12,6 +12,7 @@ val create : int -> gameboard
 (** [to_tile x] is the tile at location x*)
 (*val to_tile : int -> 'a*)
 
-(** [next_tile tile] is the list of the tiles in its path [tile]*)
+(** [next_tile tile func board] is the list of adjacent tiles to [tile]
+    [func tile1 tile2] is a function used to compare tiles *)
 val next_tile : Tile.tile -> (Tile.tile-> Tile.tile -> bool)-> 
   gameboard -> Tile.tile list
