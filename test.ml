3087632a577cd68430b344243fe3a31335d5e91e
En Yu Zhang <enyuzhang@dhcp-vl2041-20435.eduroam.cornell.edu>
2020-10-23 09:58:48 -0400

    started with tests

--- test.ml
+++ test.ml
@@ -5,9 +5,19 @@ open Player
 open Playerstate
 open Tile 
 
+let e = create_event "Day 1" "school starts" "You are now a student at Cornell" []
+let started = create_tile Green e "Start"
+let dummy = init_state "Jason" started
+let get_player_name_test 
+    (name : string)  
+    (st: st)
+    (expected_output : string) : test = 
+  name >:: (fun _ -> 
+      assert_equal expected_output (get_name st))
+
 let suite =
   "test suite for game"  >::: List.flatten [
-
+    get_player_name_test "Works?" "Jason" dummy;
   ]
 
 let _ = run_test_tt_main suite
