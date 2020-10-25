b8d3fe9e3e00079c4ffad28267ea154c2cdac824
Iris <icl5@cornell.edu>
2020-10-24 21:01:27 -0400

    revised implementation of go

--- board.ml
+++ board.ml
@@ -5,6 +5,7 @@ open Tile
 type gameboard = (Tile.tile * (Tile.tile list)) list
 
 exception No_Tile of string
+
 (** TODO: helper for generation tiles and path randomly *)
 (* let rand_paths = failwith "not_found" *)
 
@@ -26,17 +27,26 @@ let test_board = [(tile,[tile2]);(tile2,[tile])]
    - automatically gives them a numerical string id by order of generation
 
    seems like json might be easiest to use
-   start_tile input =
-   parse json |> member "start_tile" |> to_string
+   List.map applies function to all elements and builds list
+   start_tile json =
+   json |> member "start_tile" |> to_string
 
    (* makes a tile with given attributes, with id = x *)
    tile_from_json j = 
+   name = json |> member "name" |> to_string
+   id = //make incremented id
+   color = let color = json |> member "color" |> to_string in
+   match color with | "red" -> Red | "blue" -> Blue | "green" -> Green
+   | "yellow" -> Yellow
+   description = json |> member "description" |> to_string
+   effects = //make effect from string
 
-    (* *)
-   stage =
-   parse json |> member "";
+    (* makes all the tiles in each stage*)
+   stage j =
+   j |> List.map tile_from_json;
 
-   (* *)
+
+   (* find stages and iterates over each stage*)
    stages input = 
    parse json |> member "stages" |> List.map func_stage
 
@@ -65,7 +75,7 @@ let test_board = [(tile,[tile2]);(tile2,[tile])]
           ]
          }
        }
-     }]]]]]]]
+     }]]]]]]]]]]]]]]]]]]]]]
      thinking we should add tile neighbors to the tile module
      Generate board using those tiles *)
 
@@ -87,6 +97,10 @@ let rec find_tile (tile : Tile.tile) func (board : gameboard) =
   | [] -> raise (No_Tile "No such tile exists in the given board")
   | (a, b) :: t -> if func a tile then b else find_tile tile func t
 
-(** [next_tile tile func board] is the list of adjacent tiles to [tile]
-    [func tile1 tile2] is a function used to compare tiles *)
+(** [next_tile tile func board] searches through the board to find the
+    tile that matches tile and gives a list of adjacent tiles. *)
+(* TODO: Think about how to optimize because search is O(n) *)
 let next_tile = find_tile
+
+let compare_tiles_id tile1 tile2 = if get_tile_id tile1 = get_tile_id tile2
+  then true else false
--- board.mli
+++ board.mli
@@ -3,22 +3,22 @@ open Tile
 (** Represents the game board which consists of tiles *)
 type gameboard
 
+(** An exception given if the tile requested was not found in the board *)
 exception No_Tile of string
 
-(** [create_board x] is a board with [x] tiles*)
+(** [create_board x] is a board to be implemented with a json.
+    Currently uses an integer as a placeholder value*)
 val create_board : int -> gameboard
 
-(** [next_tile cur next game] determines if *)
-(*val valid_move : Tile.tile -> Tile.tile -> gameboard -> bool*)
-
-(** [to_tile x] is the tile at location x*)
-(*val to_tile : int -> 'a*)
-
-(** [start_tile board] is the first tile of the board. *)
-val start_tile : gameboard -> Tile.tile
+(** [start_tile board] is the first tile of the board.
+    Raises: No_Tile if [board] has no starting tile *)
+val start_tile : gameboard -> tile
 
 (** [next_tile tile func board] is the list of adjacent tiles to [tile]
-    [func tile1 tile2] is a function used to compare tiles *)
+    [func tile1 tile2] is a function used to compare tiles.
+    Raises: No_Tile if [tile] is not in [board] *)
 (* TODO: think about rep invariant and abstraction function*)
-val next_tile : Tile.tile -> (Tile.tile-> Tile.tile -> bool)-> 
-  gameboard -> Tile.tile list
+val next_tile : tile -> (tile-> tile -> bool) -> 
+  gameboard -> tile list
+
+val compare_tiles_id : tile -> tile -> bool
