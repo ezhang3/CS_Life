902775745476bbba3934fdd1eef7c85a029d35aa
En Yu Zhang <ez96@cornell.edu>
2020-11-13 20:57:49 -0500

    playerstate testing

--- test.ml
+++ test.ml
@@ -27,6 +27,19 @@ let tile_event_name_test (name : string) tile (expected: string) =
 let tile_description_test (name : string) tile (expected : string) = 
   name >:: (fun _ -> assert_equal expected (get_tile_description tile))
 
+(* type effect =
+    None
+   | Points of (string * int)
+   | Minigame of string
+   | Study_Partner of int *)
+
+(* let rec extract_effects_list acc (lst : effect list) = match lst with 
+   | [] -> acc
+   | h :: t -> begin 
+   match h with 
+   | Points of (x,y) -> 
+   end  *)
+
 let tile_effects_test (name : string) tile (expected : effect list) = 
   name >:: (fun _ -> assert_equal expected (get_tile_effects tile))
 
@@ -54,9 +67,11 @@ let start_tile_test (name : string) (board : gameboard) (expected : string) =
 let end_tile_test (name : string) (board: gameboard) (expected: string) =
   name >:: (fun _ -> assert_equal expected (get_tile_id (end_tile board)))
 
-let rec tile_id_list acc (lst: tile list)= match lst with
+(* helper for comparing Tile.tile lists, get tile_id list for non-abstract 
+   types,easier comparison *)
+let rec tile_id_list acc (lst: Tile.tile list)= match lst with
   | [] -> acc
-  | h :: t -> tile_id_list (get_tile_id h :: acc) t
+  | h :: t -> tile_id_list (Tile.get_tile_id h :: acc) t
 
 let next_tile_test (name : string) tile compare board (expected) =
   let next_tiles_id_list = tile_id_list [] (next_tiles tile compare board) in 
@@ -87,7 +102,7 @@ let get_player_name_test
     (st: player)
     (expected_output : string) : test = 
   name >:: (fun _ -> 
-      assert_equal expected_output (get_name st))
+      assert_equal expected_output (get_name st))     
 
 let get_points_test 
     (name: string)
@@ -95,6 +110,45 @@ let get_points_test
     (expected_output: int) : test = 
   name >:: (fun _ ->
       assert_equal expected_output (get_points st))
+
+let get_study_partners_test 
+    (name: string)
+    (st: player)
+    (expected_output: study_partners) : test = 
+  name >:: (fun _ ->
+      assert_equal expected_output (get_study_partners st))
+
+let get_project_test 
+    (name: string)
+    (st: player)
+    (expected_output: project) : test = 
+  name >:: (fun _ ->
+      assert_equal expected_output (get_project st))
+
+let get_current_tile_test 
+    (name: string)
+    (st: player)
+    (expected_output: tile_id) : test = 
+  name >:: (fun _ ->
+      assert_equal expected_output (st 
+                                    |> get_current_tile 
+                                    |> get_tile_id))
+
+let get_visited_tiles_test 
+    (name: string)
+    (st: player)
+    (expected_output: tile_id list) : test = 
+  name >:: (fun _ ->
+      assert_equal expected_output (st 
+                                    |> get_visited_tiles
+                                    |> tile_id_list []))
+
+let get_items_test 
+    (name: string)
+    (st: player)
+    (expected_output: string list) : test = 
+  name >:: (fun _ ->
+      assert_equal expected_output (get_items st))      
 
 let go_test (name : string) player board moves (expected: string) = 
   name >:: (fun _ -> go player board moves; 
@@ -105,7 +159,13 @@ let go_test (name : string) player board moves (expected: string) =
 let player_state_test = [
   get_player_name_test "Works?" test_player "Jason";
   get_points_test "Just started, 0" test_player 0; 
+  get_current_tile_test "On start" test_player "start";
+  get_visited_tiles_test "visited start only" test_player ["start"];
+  (* get_items "no items yet" test_player []; *)
+  (* get project/get study partners tests once fully implemented *)
   go_test "go test 1 move" test_player test_board 1 "tile 1";
+  (* get_current_tile_test "moved one to tile 1" test_player "tile 1";
+     get_visited_tiles_test "start and tile1" test_player ["start"; "tile 1"]; *)
   (* go_test "go test 2 moves" test_player test_board 1 "1110"; *)
 ]
 
